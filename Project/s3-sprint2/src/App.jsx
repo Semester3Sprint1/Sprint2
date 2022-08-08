dec298ac78376ec12d1fdb0888f074f370a50e61
Mike Wadden <mike.wadden@keyin.com>
2022-08-08 18:29:44 -0230

    app routes updated

--- Project/s3-sprint2/src/App.jsx
+++ Project/s3-sprint2/src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./Components/Main/Nav";
-import React, { Fragment } from "react";
+import React, { Fragment, useContext, useState } from "react";
 import Main from "./Components/Main/Main";
 import Movies from "./Components/Movies/Movies";
 import Account from "./Components/Account/Account";
 import Auth from "./Components/Account/Auth/Auth";
 import Footer from "./Components/UI/Footer";
+import AuthContext from "./Components/Context/auth-context";
 
 function App() {
+
+  const authCtx = useContext(AuthContext);
+
+
+  
   return (
     <Fragment>
       <NavBar />
@@ -16,8 +22,14 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/movies" element={<Movies />}></Route>
-          <Route path="/account" element={<Account />}></Route>
-          <Route path="/auth" element={<Auth />}></Route>
+          {authCtx.isLoggedIn &&(
+            <Route path="/account" element={<Account />}></Route>
+          )}
+          {!authCtx.isLoggedIn &&(
+            <Route path="/auth" element={<Auth />}></Route>
+          )}
+          <Route path="*" element={<Main/>}/>
+          <Route path="/footer" element={<Footer/>}/>
         </Routes>
         <Footer />
       </div>
