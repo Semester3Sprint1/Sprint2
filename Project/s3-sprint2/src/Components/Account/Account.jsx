602e99febba251421d8b86c0a4d67e6d31021704
Tarsolan <alex.ridgeley@keyin.com>
2022-08-08 11:03:50 -0230

    building folder structure

---
+++ Project/s3-sprint2/src/Components/Account/Account.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/* 
+--- COMPONENTS TO ADD ---
+Account Display Page
+
+Log In
+Log Out
+
+Create Account
+Edit Account
+*/
+
+const Account = () => {
+  return <div>Account</div>;
+};
+
+export default Account;
