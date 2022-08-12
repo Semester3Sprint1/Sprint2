529f215d4ec5b174ceca0c5c37aadbf23387844b
Mike Wadden <mike.wadden@keyin.com>
2022-08-12 08:46:24 -0230

    fixed login issue on backend side

--- Project/server/routes/auth.js
+++ Project/server/routes/auth.js
@@ -13,7 +13,7 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (!user) return res.status(400).send("Invalid email or password");
 
-  const validPassword = bcrypt.compare(req.body.password, user.password);
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
 
   if (!validPassword) return res.status(400).send("Invalid email or password");
 
