d24259921f59df8697958901347fdbf0e2d17508
Mike Wadden <mike.wadden@keyin.com>
2022-08-12 22:37:59 -0230

    nav DB button styleing

--- Project/s3-sprint2/src/Components/Services/ChangeDB.jsx
+++ Project/s3-sprint2/src/Components/Services/ChangeDB.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import {SiMongodb} from 'react-icons/si'
+import {SiPostgresql} from 'react-icons/si'
 import styles from "./css/changeDB.module.css";
-
 const ChangeDB = ({ databasePackage }) => {
   const { useMongo, setUseMongo } = databasePackage;
   const navigate = useNavigate();
   const goToMovies = () => navigate("/movies");
-
+  
   const changeDB = (status) => {
     goToMovies();
     setUseMongo(status);
   };
-
+  let styled = `btn btn-outline-light btn-lg me-2 `
   return (
-    <div className="btn-group" role="group" aria-label="Basic example">
+    <div className="" role="group" aria-label="Basic example">
       <button
         type="button"
-        className={useMongo ? `btn btn-secondary active` : `btn btn-secondary`}
+        className={useMongo ? ` ${styled + "active"}` : `${styled}`}
         onClick={() => changeDB(true)}
       >
-        MongoDB
+         <icon  className="mr-1"><SiMongodb size={30}/></icon>MongoDB
       </button>
       <button
         type="button"
-        className={!useMongo ? `btn btn-secondary active` : `btn btn-secondary`}
+        className={!useMongo ? `${styled + "active"}` : `${styled}`}
         onClick={() => changeDB(false)}
       >
-        PostgreSQL
+          <icon  className="mr-1"><SiPostgresql 
+          size={30}
+          /></icon>PostgreSQL
       </button>
     </div>
   );
