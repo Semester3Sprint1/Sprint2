82bff4455627ec6e719a6e0e71d1a73e58544b6c
Mike Wadden <mike.wadden@keyin.com>
2022-08-14 20:03:54 -0230

    added search mongoose

--- Project/server/routes/search.js
+++ Project/server/routes/search.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
-
+const winston = require("winston");
+const { Search, validate } = require("../models/search");
 const { searchMovies } = require("../services/mongo_dal/searchMovies.dal");
 
 router.post("/mongo", async (req, res) => {
-  let response = await searchMovies(req.body);
+  const { searchText } = req.body;
+  const { error } = await validate(req.body);
+  if (error) {
+    winston.error("invalid Search");
+    return res.status(400).send(error.details[0].message);
+  }
+  let response = await Search.find({ $text: { $search: `"${searchText}"` } });
+  // let response = await searchMovies(req.body);
   res.status(200).send(response);
 });
 
