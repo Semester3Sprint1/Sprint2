89ae81181041025c5721e38ae29961061bc50694
Tarsolan <alex.ridgeley@keyin.com>
2022-08-15 02:08:32 -0230

    search

--- Project/server/services/mongo_dal/searchMovies.dal.js
+++ Project/server/services/mongo_dal/searchMovies.dal.js
@@ -1,13 +1,39 @@
 const { client } = require("./mongo");
+// const assert = require("assert");
 
 const searchMovies = async (body) => {
   const { searchText } = body;
   await client.connect();
-  const cursor = client
-    .db("sample_mflix")
-    .collection("movies")
-    .find({ $text: { $search: `"${searchText}"` } });
-  const results = await cursor.toArray();
+  DEBUG && console.log(searchText);
+  const cursor = client.db("sample_mflix").collection("movies");
+  //.find({ title: { $regex: `${searchText}`, $options: "i" } });
+  //.find({ $text: { $search: `"${searchText}"` } });
+  const agg = [
+    {
+      $search: {
+        index: "searchBar",
+        compound: {
+          should: [
+            {
+              autocomplete: {
+                query: `${searchText}`,
+                path: "title",
+                fuzzy: { maxEdits: 2, prefixLength: 3, maxExpansions: 256 },
+              },
+              autocomplete: {
+                query: `${searchText}`,
+                path: "fullplot",
+                fuzzy: { maxEdits: 1, prefixLength: 4, maxExpansions: 256 },
+              },
+            },
+          ],
+        },
+      },
+    },
+    { $sort: { score: { $meta: "textScore" } } },
+  ];
+
+  const results = await cursor.aggregate(agg).toArray();
   DEBUG && console.log(results);
   DEBUG && console.log("Total results:", results.length);
 
