b81e2b5eb61895c1ccb71ab709dc59f672b35f7c
Cody <91859452+CrackedDev@users.noreply.github.com>
2022-08-17 14:47:29 -0230

    .

--- Project2/index.js
+++ Project2/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
+const { treeify, inputvalues } = require("./services/binaryTree");
 const PORT = process.env.PORT || 4000;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
@@ -10,12 +14,20 @@ app.listen(PORT, () => {
 });
 
 app.set("view engine", "ejs");
+
 app.get("/", async (req, res) => {
   res.render("input", { status: app.locals.status });
 });
 
 app.get("/proc", async (req, res) => {
-  res.render("proc");
+  res.render("proc", { status: app.locals.status });
+});
+
+app.post("/proc", function (req, res) {
+  var tree = treeify(req.body.inputvalues);
+  var StringArray = JSON.stringify(tree, null, 2);
+  console.log(StringArray);
+  res.render("proc", { StringArray, tree });
 });
 
 app.get("/records", async (req, res) => {
