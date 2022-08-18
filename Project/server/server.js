2ed79aee8a51f4429a1fa15e018e282ac074633b
Tarsolan <alex.ridgeley@keyin.com>
2022-08-18 15:28:11 -0230

    versioning & readme

--- Project/server/server.js
+++ Project/server/server.js
@@ -1,3 +1,49 @@
+/*********************************************************************************************************************\
+
+Software Development, Semester 3, Sprint Project 2 - Group Project
+
+Developer: Alex Ridgeley, Mike Wadden, Cody Barrett
+Date: August 2022
+
+Objective:
+
+To create a search engine using a sample database & generated mock data. We decided on the pre-build movie database.
+
+You'll need to use both MongoDB and postgreSQL to make proper use of this site. There is a backup
+of the PG database and a copy of several CSV files that you can import into Mongo.
+
+
+TO RUN THE APP:
+
+1)  Install postgreSQL and MongoDB. Create a database and restore the backup DB found in the 'BackUp' folder.
+    Create a MongoDB database called 'sample_mflix', and add the following collections: **IF YOU RENAME THE DATABASE, YOU WILL NEED TO UPDATE THE CODE**
+      'movies'    <----------- import the 'movies.csv' file here
+      'reviews'   <----------- import the 'reviews.csv' file here
+
+2)  Inside the 'server' folder, create a new file called '.env'. Paste the following into that new file, replacing the placeholder data with your info:
+
+URI="<your MongoDB connect URI>"
+URI2="<your MongoDB connect URI>/<the database name>"
+
+HOST="<the host name for your postgreSQL database>"
+USER="<your user account name in pgAdmin>"
+DB_PORT=<the port where you run your database>
+PASSWORD="<user password for postgres>"
+DATABASE="<the name of your postgres database>"
+
+PORT=<the port you on which want the server to run> (recommend 3001)
+
+3)  In your terminal, navigate to the 'server' folder and run this command:
+          npm i         <----------- This will install the node_modules necessary to run the server.
+
+4)  After this is complete, navigate to the 'client' folder, and run the following two commands to start everything:
+          npm i
+          npm run dev
+
+This will run both of the modules at once, and the React app should load in a browser window.
+
+\*********************************************************************************************************************/
+
 const express = require("express");
 const app = express();
 const error = require("./middleware/error");
